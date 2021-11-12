138e044155e7ca8ec276e200944f2be6a524f26d
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2021-11-11 23:11:59 -0800

    Time: 200 ms (56.01%), Space: 66 MB (61.84%) - LeetHub

---
+++ palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+  let slow = head;
+  let fast = head;
+  
+  while(slow.next && fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  
+  let a = head;
+  let bHead = reverse(slow.next);
+  let b = bHead;
+  
+  let ans = true;
+  while(a && b) {
+    if(a.val !== b.val) {
+      ans = false;
+      break;
+    };
+    a = a.next;
+    b = b.next;
+  }
+  
+  reverse(bHead);
+  
+  return ans;
+};
+
+function reverse(head) {
+  let prev = null;
+  let curr = head;
+  let next = null;
+  
+  while(curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  
+  return prev;
+};
+

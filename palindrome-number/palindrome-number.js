1f17524f80e5fb30833e4d17203a8a50d5ef20c3
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2021-11-22 20:44:19 -0800

    Time: 184 ms (69.59%), Space: 47.6 MB (95.17%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    let array = [];
+    
+    if (x < 0) {
+        return false;
+    }
+    
+    while (x > 0) {
+        const y = x % 10;
+        array.push(y);
+        x = Math.floor(x / 10);
+    }
+    
+    return JSON.stringify(array) === JSON.stringify(array.reverse())
+};

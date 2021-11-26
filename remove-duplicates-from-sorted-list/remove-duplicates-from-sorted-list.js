626dd3391e0c040d3555c1cec1a39058a221b75e
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2021-11-26 14:37:13 -0800

    Time: 80 ms (91.47%), Space: 40.7 MB (49.87%) - LeetHub

---
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    // o : linked list with no duplicates
+    // i : sorted linked list with potential duplicates
+    // c : return the linked list sorted
+    // e : is the linked list contains only one element? return the same linked list
+    
+    let current = head;
+    
+    while (current && current.next) {
+        if (current.val === current.next.val) {
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+        }
+    }
+    
+    return head;
+};

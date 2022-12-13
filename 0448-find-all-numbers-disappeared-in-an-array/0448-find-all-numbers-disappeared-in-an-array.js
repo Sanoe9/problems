09af94de97d8ab7faf54ff4929d1a67ddf5a84a3
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2022-12-13 12:37:42 -0800

    Time: 7688 ms (23.83%), Space: 57.4 MB (15.35%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.js
@@ -3,13 +3,12 @@
  * @return {number[]}
  */
 var findDisappearedNumbers = function(nums) {
-    let range = [...Array(nums.length + 1).keys()];
-    console.log('range', range);
     let result = [];
-    for (let i = 1; i < range.length; i++) {
-        let num = range[i];
-        if (!nums.includes(num)) {
-            result.push(num);
+    const range = [...Array(nums.length + 1).keys()].slice(1);
+    for (let i = 0; i < range.length; i++) {
+        const element = range[i];
+        if (!nums.includes(element)) {
+            result.push(element);
         }
     }
     return result;

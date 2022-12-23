3cf64ac2107305c0ba0d25ced81a5aec70aeb63d
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2022-12-22 16:17:10 -0800

    Time: 97 ms (81.02%), Space: 43 MB (32.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+      let result = [];
+      let obj = {};
+      for (let i = 0; i < nums.length; i++) {
+        const number = nums[i];
+        if (obj[target - number] !== undefined) {
+          return [obj[target - number], i];
+        }
+        obj[number] = i;
+      }
+      return result;
+};

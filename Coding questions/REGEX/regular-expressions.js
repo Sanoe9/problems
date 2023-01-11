b25e8412930c8a604f2244c7d2d21dac7aa54223
Salma Noe <sattari2101@gmail.com>
2023-01-11 15:43:08 -0800

    Use i flag to ignore caps in regex

--- Coding questions/REGEX/regular-expressions.js
+++ Coding questions/REGEX/regular-expressions.js
@@ -7,3 +7,9 @@ let result = myRegex.test(myString);
 let petString = "James has a pet cat.";
 let petRegex = /dog|cat|bird|fish/;
 let result = petRegex.test(petString);
+
+////////////////////////////////////////////////
+
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i;
+let result = fccRegex.test(myString);

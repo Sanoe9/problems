c981c6ce6a733d002c6f6a81b56d769ef1572d88
Salma Noe <sattari2101@gmail.com>
2023-01-12 19:08:12 -0800

    Solve regex problem using global flag

--- Coding questions/REGEX/regular-expressions.js
+++ Coding questions/REGEX/regular-expressions.js
@@ -19,3 +19,9 @@ let result = fccRegex.test(myString);
 let extractStr = "Extract the word 'coding' from this string.";
 let codingRegex = /coding/;
 let result = extractStr.match(codingRegex);
+
+////////////////////////////////////////////////
+
+let twinkleStar = "Twinkle, twinkle, little star";
+let starRegex = /twinkle/ig; // Change this line
+let result = twinkleStar.match(starRegex); // Change this line 

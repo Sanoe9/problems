cb87bd08378ef1b2be33012c229db222e6e61a4c
Salma Noe <sattari2101@gmail.com>
2023-03-20 12:25:30 -0700

    Solve maxProfit using sliding window

--- best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,11 +3,20 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    let maxProfit = 0;
-    let min  = prices[0];
-    for (let i = 1; i < prices.length; i++) {
-        min = Math.min(prices[i], min);
-        maxProfit = Math.max(maxProfit, prices[i] - min);
+    let profit = 0;
+    let buy = prices[0];
+    let sell = prices[1];
+    
+    for (let i = 0; i < prices.length; i++) {
+        if (buy > sell) {
+            buy = prices[i + 1];
+            sell = prices[i + 2];
+        }
+        if (buy <= sell) {
+            profit = Math.max(profit, sell - buy);
+            sell = prices[i + 2];
+        }
     }
-    return maxProfit;
+    
+    return profit;
 };

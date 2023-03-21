deeecd7d6c03fb0c642c16d45cd4d3d3d494c73b
Salma Noe <sattari2101@gmail.com>
2023-03-21 15:32:07 -0700

    Solve place flowers problem

---
+++ 605-Can-Place-Flowers/605-Can-Place-Flowers.js
@@ -0,0 +1,17 @@
+var canPlaceFlowers = function(flowerbed, n) {
+    let possibleFlowers = 0;
+    
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (flowerbed[i] === 1) {
+            continue;
+        }
+        if ((i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
+            flowerbed[i] = 1;
+            possibleFlowers++;
+        }
+    }
+    
+    return possibleFlowers >= n;
+};
+
+console.log(canPlaceFlowers([0], 1));

14ad4a9e6c09cf9a6eda39abf0aea6f4a5684cb7
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2023-03-21 15:31:25 -0700

    Time: 73 ms (31.56%), Space: 44.8 MB (14.54%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -5,28 +5,14 @@
  */
 var canPlaceFlowers = function(flowerbed, n) {
 let possibleFlowers = 0;
-    let prev;
-    let next = flowerbed[1];
-    let curr = flowerbed[0];
     
     for (let i = 0; i < flowerbed.length; i++) {
-        if ((prev === undefined || prev === 0) && curr === 0 && next === 0 || (prev === undefined && curr === 0 && next === undefined)) {
-            console.log(i, 'first if');
+        if (flowerbed[i] === 1) {
+            continue;
+        }
+        if ((i === 0 || flowerbed[i - 1] === 0) && (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)) {
             flowerbed[i] = 1;
             possibleFlowers++;
-            prev = flowerbed[i];
-            curr = flowerbed[i + 1];
-            next = flowerbed[i + 2];
-        } else if (prev === 0 && curr === 0 && next === undefined) {
-            console.log(i,'second if');
-            flowerbed[i] = 1;
-            possibleFlowers++;
-            return possibleFlowers >= n;
-        } else {
-            console.log(i,'third if');
-            prev = flowerbed[i];
-            curr = flowerbed[i + 1];
-            next = flowerbed[i + 2];
         }
     }
     

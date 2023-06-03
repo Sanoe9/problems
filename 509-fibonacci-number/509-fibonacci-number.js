ef0db9fbdfcd94a468057f9040687ca902ee9036
Salma Noe <sattari2101@gmail.com>
2023-06-03 09:23:23 -0700

    Solve fibonacci sequence using recursion and DP

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,25 @@
+// using recursion
+
+var fib = function(n) {
+    // base cases
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    
+    // recursive case
+    return fib(n - 1) + fib(n - 2);
+};
+
+// using DP
+const fibDp = (n) => {
+    let dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
+}

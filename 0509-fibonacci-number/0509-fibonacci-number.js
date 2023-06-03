362d9727e4e16e0b9be1b56b62403892362ac38a
Salma Noe <69939272+Sanoe9@users.noreply.github.com>
2023-06-03 09:21:41 -0700

    Time: 53 ms (87.83%), Space: 41.7 MB (70.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -4,13 +4,20 @@
  */
 var fib = function(n) {
     // base cases
-    if (n === 0) {
-        return 0;
-    }
-    if (n === 1) {
-        return 1;
-    }
+//     if (n === 0) {
+//         return 0;
+//     }
+//     if (n === 1) {
+//         return 1;
+//     }
     
-    // recursive case
-    return fib(n - 1) + fib(n - 2);
+//     // recursive case
+//     return fib(n - 1) + fib(n - 2);
+    let dp = [];
+    dp[0] = 0;
+    dp[1] = 1;
+    for (let i = 2; i <= n; i++) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+    return dp[n];
 };

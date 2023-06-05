ed4484561dbfd27248c833bee300f4b7d005a4a7
Salma Noe <sattari2101@gmail.com>
2023-06-04 20:40:40 -0700

    Solve concatenation

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.js
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.js
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 /**
  * @param {number[]} nums
  * @return {number[]}
  */
-=======
->>>>>>> d08e0e5 (Solve buildArray problem)
 var buildArray = function(nums) {
     let ans = [];
     for (let i = 0; i < nums.length; i++) {

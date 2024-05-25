4f82895cbfe78d2a8d4d78c9874e7f282556f774
Salma Noe <sattari2101@gmail.com>
2024-05-24 19:10:40 -0700

    Solve twoSums

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,6 +3,21 @@
  * @param {number} target
  * @return {number[]}
  */
+
+const twoSum1 = (nums, target) => {
+  let result = [];
+  for (let i = 0; i <= nums.length - 1; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] === target) {
+              result.push(i, j);
+              return result;
+          }
+      }
+  }
+
+  return result;
+}
+
 var twoSum = function(nums, target) {
   let result = [];
   let obj = {};

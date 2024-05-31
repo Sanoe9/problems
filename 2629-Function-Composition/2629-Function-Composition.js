1212ca785cfcd0f709f0521c20027d840f7cd1ef
Salma Noe <sattari2101@gmail.com>
2024-05-31 12:25:45 -0700

    Solve function compose problem

---
+++ 2629-Function-Composition/2629-Function-Composition.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    return functions.reduce((acc, fn) => {
+        return x => acc(fn(x))
+    }, x => x)
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
